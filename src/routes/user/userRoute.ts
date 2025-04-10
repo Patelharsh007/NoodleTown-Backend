5b4db0770ee3ff5ccbc55eb5b0a112c2d4686645
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-10 14:21:27 +0530

    Refactor userRoute to include address management routes and ensure authentication middleware is applied

--- src/routes/user/userRoute.ts
+++ src/routes/user/userRoute.ts
@@ -1,11 +1,36 @@
 import express from "express";
 
 import { authenticateUserMiddleware } from "../../middlewares/authenticateUserMiddleware";
-import { verifyUser } from "../../controllers/userController";
+import {
+  getAddresses,
+  addAddress,
+  updateAddress,
+  deleteAddress,
+} from "../../controllers/addressContoller";
+import { verifyUser, updatePassword } from "../../controllers/userController";
 
 const router = express.Router();
 
+// router.use(authenticateUserMiddleware);
+
 // router.get("/all", verifyTokenMiddleware, getUsers);
 router.get("/verifyUser", authenticateUserMiddleware, verifyUser);
+
+router.use(authenticateUserMiddleware);
+
+//update password
+router.post("/updatePassword", updatePassword);
+
+//get all user address
+router.get("/addresses", getAddresses);
+
+//add new user address
+router.post("/addAddress", addAddress);
+
+//update user address
+router.patch("/updateAddress/:addressId", updateAddress);
+
+//delete user address
+router.delete("/deleteAddress/:addressId", deleteAddress);
 
 export const userRouter = router;
