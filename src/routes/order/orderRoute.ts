6ee414a782fadd5d9dbf0a6dc1fa5e15718c957e
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-14 17:23:14 +0530

    Add order routes with authentication middleware for order management

--- src/routes/order/orderRoute.ts
+++ src/routes/order/orderRoute.ts
@@ -1,2 +1,17 @@
 import express from "express";
 import { authenticateUserMiddleware } from "../../middlewares/authenticateUserMiddleware";
+import {
+  createOrderAndPayment,
+  verifyStripePayment,
+  getOrdersbyUser,
+} from "../../controllers/orderController";
+
+const router = express.Router();
+
+router.use(authenticateUserMiddleware);
+
+router.post("/createOrder", createOrderAndPayment);
+router.get("/verifyPayment", verifyStripePayment);
+router.get("/getOrders", getOrdersbyUser);
+
+export const orderRouter = router;
