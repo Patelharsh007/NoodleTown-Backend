068f97bb5f1e45d6534bc48835f0fb3c7a71a539
Patelharsh007 <ptlharsh3073@gmail.com>
2025-04-21 13:41:11 +0530

    remove OPTIONS method from CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ app.use(
       "http://localhost:3002",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
